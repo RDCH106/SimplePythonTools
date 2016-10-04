ca6def51d217199667bc91ebb88513b2b74bde20
Rubén de Celis Hernández <rubendecelis@ingran.es>
2016-10-04 12:19:03 +0200

    # Fix ANSI color in Windows 10 version 10.0.14393 (Windows Anniversary Update)

--- common.py
+++ common.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import platform
 
 def cleanScreen():
     if os.name == 'nt':
@@ -38,6 +39,12 @@ def countLines(file):
         print("File Not Found!")
     finally:
         return count
+
+if os.name == 'nt' and platform.release() == '10' and platform.version() >= '10.0.14393':
+    # Fix ANSI color in Windows 10 version 10.0.14393 (Windows Anniversary Update)
+    import ctypes
+    kernel32 = ctypes.windll.kernel32
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 class bcolors:
     HEADER = '\033[95m'
