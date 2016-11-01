659882e50a4b2519f7647d39a51991bf95456325
Rubén de Celis Hernández <rubendecelis@ingran.es>
2016-11-02 00:57:14 +0100

    Test multiline comments

--- managers/jsonManager.py
+++ managers/jsonManager.py
@@ -5,12 +5,11 @@ import collections
 import pathlib
 import inspect
 
-"""
-This python 3 class will manager json individually for you.
+"""This python 3 class will manager json individually for you.
 
 To generate HTML documentation for this module issue the command:
 
-pydoc -w jsonManager
+    pydoc -w jsonManager
 """
 
 class JsonManager(object):
