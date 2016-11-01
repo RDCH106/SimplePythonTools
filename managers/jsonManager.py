af992c71a58fa0790c8b5d1a2402a887384fed3e
vgonisanz <vgonisanz@gmail.com>
2016-11-01 16:07:32 +0100

    Reimplemented classmethods

--- managers/jsonManager.py
+++ managers/jsonManager.py
@@ -25,7 +25,7 @@ class JsonManager(object):
     """
     Initialize JsonManager with a name to identify eash instance
     """
-    #@classmethod
+    @classmethod
     def __init__(self, name):
         """
         Construct a new 'JsonManager' object.
@@ -40,7 +40,7 @@ class JsonManager(object):
     """
     Print parser ID
     """
-    #@classmethod
+    @classmethod
     def print_ID(self):
         """
         Prints json Manager ID to the display.
@@ -51,7 +51,7 @@ class JsonManager(object):
     """
     Print all methods
     """
-    #@classmethod
+    @classmethod
     def print_methods(self):
         print(inspect.getmembers(self, predicate=inspect.ismethod))
         return
@@ -59,7 +59,7 @@ class JsonManager(object):
     """
     Test function
     """
-    #@classmethod
+    @classmethod
     def test(self):
         print("Test")
         print(inspect.getmembers(self, predicate=inspect.ismethod))
@@ -68,7 +68,7 @@ class JsonManager(object):
     """
     set_key_array
     """
-    #@classmethod
+    @classmethod
     def set_key_array(self, key_array = _key_array):
         print("Setting keys: %s" % list(key_array) )
         _key_array = key_array
@@ -77,7 +77,7 @@ class JsonManager(object):
     """
     set_input_path
     """
-    #@classmethod
+    @classmethod
     def set_input_path(self, input_path = None):
         if input_path is None:
             input_path = self.input_path
@@ -88,7 +88,7 @@ class JsonManager(object):
     """
     set_output_path
     """
-    #@classmethod
+    @classmethod
     def set_output_path(self, output_path = None):
         if output_path is None:
             output_path = self.output_path
@@ -108,7 +108,7 @@ class JsonManager(object):
     """
     process_function_default
     """
-    #@classmethod
+    @classmethod
     def process_function(self, *arg):
         if(self._function_callback == None):
             print("Default process... overload call with set_process_function!")
@@ -120,7 +120,7 @@ class JsonManager(object):
     """
     print_json_value
     """
-    #@classmethod
+    @classmethod
     def print_json_value(self, json_data = None, key = None):
         value = None
         if json_data is None:
@@ -135,7 +135,7 @@ class JsonManager(object):
     """
     print_all_json_values
     """
-    #@classmethod
+    @classmethod
     def print_all_json_values(self, json_data = None):
         if json_data is None:
             json_data = self._json_data
@@ -147,7 +147,7 @@ class JsonManager(object):
     """
     print_all_json_values_in_an_array
     """
-    #@classmethod
+    @classmethod
     def print_all_json_values_in_an_array(self, array_json_data = None):
         if array_json_data is None:
             array_json_data = self._array_json_data
@@ -162,7 +162,7 @@ class JsonManager(object):
     """
     print_arraykey_json_values
     """
-    #@classmethod
+    @classmethod
     def print_arraykey_json_values(self, json_data = None, array_key = None):
         if json_data is None:
             json_data = self._json_data
@@ -179,7 +179,7 @@ class JsonManager(object):
     :return: successful
     :return: _json_data data in json
     """
-    #@classmethod
+    @classmethod
     def read_json_from_file(self, file_name = None):
         successful = False
         json_data = json.dumps({})
@@ -198,7 +198,7 @@ class JsonManager(object):
     """
     Write json
     """
-    #@classmethod
+    @classmethod
     def write_json(self, file_name, json_data):
         print("Writing: %s" % file_name)
         full_destination_name = os.path.join(self._output_path, file_name)
@@ -209,7 +209,7 @@ class JsonManager(object):
     """
     Get value from json
     """
-    #@classmethod
+    @classmethod
     def get_value(self, json_data = None, key = None):
         value = None
         if json_data is None:
@@ -223,7 +223,7 @@ class JsonManager(object):
     """
     create_json_object_from_list
     """
-    #@classmethod
+    @classmethod
     def create_json_object_from_list(self, list_of_list = None, print_values = False):
         successful = True
         data = {}
@@ -236,7 +236,7 @@ class JsonManager(object):
     """
     create_json_object_from_csv
     """
-    #@classmethod
+    @classmethod
     def create_json_object_from_csv(self, csv_name, split_char = ';', print_values = False):
         print("Reading csv file: %s" % csv_name)
         full_source_name = os.path.join(self._input_path, csv_name)
@@ -258,7 +258,7 @@ class JsonManager(object):
     """
     create_json_object_from_csv_with_title
     """
-    #@classmethod
+    @classmethod
     def create_json_object_from_csv_with_title(self, csv_name, split_char = ';', print_values = False):
         print("Reading csv file: %s" % csv_name)
         full_source_name = os.path.join(self._input_path, csv_name)
@@ -289,7 +289,7 @@ class JsonManager(object):
     """
     edit_json_from_lists
     """
-    #@classmethod
+    @classmethod
     def edit_json_from_lists(self, json_data = None, list_of_list = None):
         if json_data is None:
             json_data = self._json_data
@@ -305,7 +305,7 @@ class JsonManager(object):
     """
     process_all_files: Require initialize process_function with set_process_function
     """
-    #@classmethod
+    @classmethod
     def process_all_files(self, path, print_files = False):
         self.set_input_path(path)
         for filename in os.listdir(path):
@@ -318,7 +318,7 @@ class JsonManager(object):
     """
     process_all_files_in_folders: Require initialize process_function with set_process_function
     """
-    #@classmethod
+    @classmethod
     def process_all_files_in_folders(self, path, print_folders = False, print_files = False):
         for root, directories, filenames in os.walk(path):
             print("Processing with root path: %s" % root)
