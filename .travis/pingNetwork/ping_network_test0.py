505d146c5332ea9b05763356ca9a7d0a69367099
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-08-03 16:10:33 +0200

    Moved pingNetwork to network package

--- .travis/pingNetwork/ping_network_test0.py
+++ .travis/pingNetwork/ping_network_test0.py
@@ -1,7 +1,7 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../")
-from ping_network import pingNetwork
+from network.ping_network import pingNetwork
 
 pingNetwork()
